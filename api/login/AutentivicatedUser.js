e973c33298ab35e2e7773f1c7935e56864b3c72f
baihaqie <baihaqiearrafi@students.polmed.ac.id>
2026-03-02 23:34:07 +0700

    update 2 backend

--- api/login/AutentivicatedUser.js
+++ api/login/AutentivicatedUser.js
@@ -2,6 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../../models/User.js";
+import admin from "../../config/firebase.js";
 
 const router = express.Router();
 
@@ -48,6 +49,98 @@ router.post("/", async (req, res) => {
                 email: user.email,
                 role: user.role
             }
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error"
+        });
+    }
+});
+
+// 🔥 GOOGLE LOGIN
+router.post("/google", async (req, res) => {
+    try {
+        const {
+            idToken
+        } = req.body;
+
+        const decodedToken = await admin.auth().verifyIdToken(idToken);
+
+        const {
+            email,
+            name
+        } = decodedToken;
+
+        let user = await User.findOne({
+            email
+        });
+
+        if (!user) {
+            user = await User.create({
+                name,
+                email,
+                password: "google-login",
+                role: "user",
+            });
+        }
+
+        const token = jwt.sign({
+                id: user._id,
+                role: user.role
+            },
+            process.env.JWT_SECRET, {
+                expiresIn: "1d"
+            }
+        );
+
+        res.json({
+            message: "Login Google berhasil",
+            token,
+            user: {
+                email: user.email,
+                role: user.role,
+            },
+        });
+
+    } catch (error) {
+        res.status(401).json({
+            message: "Google token tidak valid"
+        });
+    }
+});
+
+
+// Manual Registrasi Cuy
+// 🔥 REGISTER MANUAL
+router.post("/register", async (req, res) => {
+    try {
+        const {
+            name,
+            email,
+            password
+        } = req.body;
+
+        const existingUser = await User.findOne({
+            email
+        });
+        if (existingUser) {
+            return res.status(400).json({
+                message: "Email sudah digunakan"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            role: "user"
+        });
+
+        res.json({
+            message: "Register berhasil"
         });
 
     } catch (error) {
